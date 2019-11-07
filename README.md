4899eaeec5c29d9e95beb84f2795f1b9d9a7d5cc
François Nguyen <fnguyen.prestataire2@oui.sncf>
2019-11-07 17:32:46 +0100

    Using actual NPM packages

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# tock-js-example
-Sample code for a chatbot coded using Tock and tock-js
+# tock-node-example
+
+Sample code for a chatbot coded using Tock and tock-node
+
+```
+yarn install
+yarn start
+
+npm i
+npm start
+```
--- src/index.js
+++ src/index.js
@@ -1,17 +1,157 @@
-const { WebBotInterface, Bot } = require('tock-js');
+const { WebBotInterface, Bot, imageCard, i18nText } = require('tock-node');
+const Unsplash = require('unsplash-js').default;
+const toJson = require('unsplash-js').toJson;
+require('isomorphic-fetch');
 
-const bot = new Bot('<APP_ID>', '<TOCK_END_POINT>', 443);
+const unsplash = new Unsplash({
+  applicationId: '<UNSPLASH_APP_ID>',
+  secret: '<UNSPLASH_SECRET>',
+});
+
+const bot = new Bot('<API_KEY>', '<TOCK_SERVER_URL>');
+
+const itemHandler = ({ send, userData }) => {
+  if (Array.isArray(userData.wishlist) && userData.wishlist.length > 0) {
+    send(
+      `You have ${userData.wishlist.length} item${
+        userData.wishlist.length > 1 ? 's' : ''
+      } in your wishlist!`,
+      {
+        title: i18nText('View my list'),
+      }
+    );
+  }
+};
 
 bot.addInterface(WebBotInterface());
 
-bot.addStory('greetings', function(botInterface) {
-  botInterface.send('Hello!');
+bot.addStory(
+  'greetings',
+  ({ send }) => {
+    send(
+      "Hello!<br>I'm a bot made in Tock. Create a wish list of anything with me!<br>Try: <i>I want an iPhone 11</i>"
+    );
+  },
+  itemHandler
+);
+
+bot.addStory('insult', ({ send }) => {
+  send('That is not nice 😥');
 });
 
-bot.addStory('insult', function(botInterface) {
-  botInterface.send('That is not nice :(');
+bot.addStory('gratitude', ({ send }) => {
+  send('Thank you 😊');
 });
 
-bot.addStory('gratitude', function(botInterface) {
-  botInterface.send('Thank you :)');
+bot.addStory('how_are_you', ({ send }) => {
+  send("I'm doing great! How about you?");
 });
+
+bot.addStory(
+  'add_wish',
+  ({ send, userData, dispatchUserData }, request) => {
+    if (request.entities.length > 0) {
+      const userItems = Array.isArray(userData.wishlist) ? userData.wishlist : [];
+      const items = request.entities
+        .filter(entity => entity.type === 'phurytw:item')
+        .map(entity => entity.content);
+      const itemsToAdd = items.reduce(
+        (prev, cur) => (!userItems.includes(cur) ? [...prev, cur] : prev),
+        []
+      );
+      if (itemsToAdd.length > 0) {
+        const wishlist = [...userItems, ...items];
+        send(
+          `You have added: ${itemsToAdd.join(', ')} to your wishlist. Your wishlist now has ${
+            wishlist.length
+          } item${wishlist.length > 1 ? 's' : ''}`
+        );
+        dispatchUserData({ wishlist });
+      } else {
+        send(`You already have ${items.length > 1 ? 'these' : 'this'} item in your wishlist.`);
+      }
+    } else {
+      send("I don't understand 😕 What were you trying to add?");
+    }
+  },
+  itemHandler
+);
+
+bot.addStory(
+  'clear_wishlist',
+  ({ send, userData, dispatchUserData }) => {
+    if (!userData.wishlist || userData.wishlist.length === 0) {
+      send('Your wishlist is already empty!');
+    } else {
+      dispatchUserData({ wishlist: [] });
+      send(
+        `Your wishlist has been emptied. (${userData.wishlist.length} item${
+          userData.wishlist.length > 1 ? 's' : ''
+        } removed)`
+      );
+    }
+  },
+  itemHandler
+);
+
+bot.addStory('request_wishlist', async ({ send, userData }) => {
+  if (Array.isArray(userData.wishlist) && userData.wishlist.length > 0) {
+    const wishlistLength = userData.wishlist.length;
+    if (wishlistLength > 1) {
+      send(`Here are your ${wishlistLength} items in your wishlist:`);
+    } else {
+      send(`You have only one item in your wishlist:`);
+    }
+    const cards = await Promise.all(
+      userData.wishlist.map(item =>
+        unsplash.search
+          .photos(item, 1, 1)
+          .then(toJson)
+          .then(data => {
+            if (data.results && data.results[0]) {
+              return imageCard(item, data.results[0].urls.thumb);
+            }
+            return undefined;
+          })
+      )
+    ).then(results => results.filter(v => v));
+    send({
+      cards,
+      type: 'carousel',
+    });
+  } else {
+    send('You have 0 item in your wishlist. Add something! 😄');
+  }
+});
+
+bot.addStory(
+  'remove_wish',
+  ({ send, userData, dispatchUserData }, request) => {
+    if (!Array.isArray(userData.wishlist) || userData.wishlist.length === 0) {
+      send('You have no item to remove. Try adding one!');
+    } else if (request.entities.filter(entity => entity.type === 'phurytw:item').length === 0) {
+      send("I don't understand 😕 What were you trying to remove?");
+    } else {
+      const userItems = Array.isArray(userData.wishlist) ? userData.wishlist : [];
+      const items = request.entities
+        .filter(entity => entity.type === 'phurytw:item')
+        .map(entity => entity.content.toLowerCase());
+      const itemsToRemove = userItems.reduce(
+        (prev, cur) => (items.includes(cur.toLowerCase()) ? [...prev, cur] : prev),
+        []
+      );
+      if (itemsToRemove.length > 0) {
+        const wishlist = userData.wishlist.filter(item => !items.includes(item));
+        dispatchUserData({ wishlist });
+        send(
+          `${itemsToRemove.length} item${
+            itemsToRemove.length > 1 ? 's' : ''
+          } has been removed from your wishlist.`
+        );
+      } else {
+        send('These items are not in your list.');
+      }
+    }
+  },
+  itemHandler
+);
